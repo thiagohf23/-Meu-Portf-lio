f4f299aaf542ebcba70e1929f0facd6160a6854e
thiagohf23 <thiiagoft25@gmail.com>
2024-11-07 22:07:12 -0300

    first commit

---
+++ portifolio/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Meu Portfólio</title>
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="bg-red-500 mx-auto max-w-screen-lg px-3 py-6">
+        <div class="italic">
+            Meu Portfólio
+        </div>
+    </header>
+</body>
+</html>

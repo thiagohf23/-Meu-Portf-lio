728770a027ff3fcff2d45f5f291818c87f64deed
thiagohf23 <thiiagoft25@gmail.com>
2024-11-07 23:31:47 -0300

    Adicionado elementos a página

--- portifolio/index.php
+++ portifolio/index.php
@@ -7,10 +7,83 @@
     <title>Meu Portfólio</title>
 </head>
 <body class="bg-gray-900 text-white">
-    <header class="bg-red-500 mx-auto max-w-screen-lg px-3 py-6">
-        <div class="italic">
-            Meu Portfólio
+    <header class=" bg-red mx-auto max-w-screen-lg items-center px-3 py-6 flex justify-between">
+        <!--- logo -->
+        <div class="font-bold text-cyan-500 text-3xl">
+         🦦 Meu Portfólio...
         </div>
-    </header>
+    <div>
+        <!--- links -->
+    <ul class="flex gap-4">
+        <li><a href="" class="hover:underline">Projetos</a></li>
+        <li><a href="" class="hover:underline">Github</a></li>
+        <li><a href="" class="hover:underline">Linkedin</a></li>
+
+    </ul>
+ </div>
+</header>
+
+
+
+
+
+    <main class="mx-auto max-w-screen-lg  minh-h-20 text-gray-950">
+
+    <!-- hero-->
+
+    <section class="flex gap-x-3 ">
+     <!-- Titulo e Descrição-->
+    <div class="w-2/3 text-gray-200">
+        <h1 class="text-2xl font-bold">Olá, meu nome e Thiago de Sá:</h1>
+        <p class="text-xl leading-8 mt-6">
+        Falando um pouco sobre mim, sou desenvolvedor web focado em criar soluções 
+        inovadoras e em aprender novas tecnologias. Tenho especialização em HTML, CSS, JavaScript e PHP.
+        
+            <ul class="flex gap-x-4 mt-3">
+            <li><a class="href">G</a></li>
+            <li><a class="href">L</a></li>
+            <li><a class="href">H</a></li>
+
+
+
+            </ul>
+
+
+        </p>
+
+    </div>
+     <!-- imagem-->
+    <div class="w-1/3  flex items-center justify-center ">
+
+       <div>FOTO</div>
+
+    </div>
+
+    </section>
+
+     <!-- projetos-->
+    
+     <section>
+
+     <!-- projetos-->
+
+     <div>
+
+
+     </div>
+
+     </section>
+    
+    
+    
+    
+    
+
+</main>
+
+
+
+
+
 </body>
 </html>
